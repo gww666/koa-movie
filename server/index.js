a53e338d2a2a86bf303fdabb5bcb97c3445b6e7d
gw666 <sggw100@126.com>
2018-03-13 14:53:17 +0800

    添加pug模板 gw

--- server/index.js
+++ server/index.js
@@ -1,6 +1,15 @@
+const {resolve} = require("path");
 const Koa = require("koa");
 const app = new Koa();
+// const pug = require("pug");
+const views = require("koa-views");
+// const {pugTpl} = require("../tpl");
+app.use(views(resolve(__dirname, "./views"), {
+    extension: "pug"
+}));
 app.use(async (ctx, next) => {
-    ctx.body = "电影首页";
+    // ctx.body = "电影首页";
+    // ctx.body = pug.render(pugTpl);
+    await ctx.render("index", {});
 });
 app.listen(3000);
---
+++ server/tpl/pug.js
@@ -0,0 +1,18 @@
+module.exports = `
+doctype html
+html
+    head
+        meta(charset="utf-8")
+        meta(name="viewport", content="width=device-width, initial-scale=1")
+        title pug test
+        link(rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/4.0.0/css/bootstrap.min.css" crossorigin="anonymous")
+        script(src="https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js")
+        script(src="https://cdn.bootcss.com/bootstrap/4.0.0/js/bootstrap.bundle.min.js")
+    body
+        .container
+            .row
+                .col-md-4 hahah
+                .col-md-8 lalala
+
+
+`
